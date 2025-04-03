42e5da06f90b2e8f2a5ecf5585f866307b6eb7ae
nick <fafaniconi@gmail.com>
2025-04-03 12:11:57 -0300

    imc com condiacao

--- LPA 1/ATIVIDADE-1/imc.c
+++ LPA 1/ATIVIDADE-1/imc.c
@@ -13,11 +13,35 @@ int main(){
     printf("Digite seu altura: ");
     scanf("%f", &altura);
     
+    float i = imc(altura, peso);
+
     printf("\n------------------------------------\n\n");
 
     printf("Peso: %.2fkg\nAltura: %.2fcm", peso, altura);
     printf("\n\nIMC: %.2f", imc(altura, peso));
     printf("\n\n------------------------------------\n\n");
+
+    if (i < 18.5){
+        printf("Abaixo do peso");
+    }
+    if (i > 18.5 && i <= 24.9){
+        printf("Peso ideal");
+    }
+    if (i > 24.9 && i <= 29.9){
+        printf("Levemente acima do peso");
+    }
+    if (i > 29.9 && i <= 34.9){
+        printf("Obesidade grau I");
+    }
+    if (i > 34.9 && i <= 39.9){
+        printf("Obesidade grau II");
+    }
+    if (i > 39.9){
+        printf("Obesidade grau III");
+    }
+    
+    printf("\n\n------------------------------------\n\n");
+    
     return 0;
 }
 
