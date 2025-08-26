afdbe709c78833d68b1be53122fe662801c0acb1
nick <fafaniconi@gmail.com>
2025-08-26 13:07:32 -0300

    pesquisa-sequencial

--- ED/EXERCICIOS/SLIDE 03/01/pl.c
+++ ED/EXERCICIOS/SLIDE 03/01/pl.c
@@ -6,5 +6,10 @@
 
 int main(int argc, char const *argv[]){
     Time * tab = lerDados("tabela.csv"); 
+    
+    for (int i = 0; i < 20; i++){
+        printf("%d\t%.2f\t%s\n", tab->posicao, tab->aproveitamento, tab->time);
+    }
+    
     return 0;
 }
