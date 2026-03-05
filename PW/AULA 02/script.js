3e9210b8b23062eeb1c0fe00335e60f8c18b1950
NICHOLAS RAFAEL QUEIROZ DOS REIS <nicholas.reis@a.ucb.br>
2026-03-05 12:51:31 -0300

    AULA 02

---
+++ PW/AULA 02/script.js
@@ -0,0 +1,38 @@
+console.log("Teste Javascript");
+
+var animal = "gato"; 
+console.log(animal);
+
+let nomeCompleto = 'Fulano da Silva';
+console.log(nomeCompleto);
+
+const valor = 10; 
+console.log(valor);
+
+var valor1; // var permite retribuição
+valor1 = 10;
+valor1 = '15';
+
+//verificacao de tipo
+console.log(typeof valor1);
+
+//algoritmo: entrada processamento saia
+
+//var nome = window.prompt("Nome: "); 
+//console.log("Seja bem vindo " + nome); 
+//document.writeln(`Seja bem-vindo ${nome}. Aproveite`); 
+
+
+//operadores aritimedicos: / * - + ** 
+
+
+//operadores de comparação: < > <= >= == != === !==
+console.log(5 == '5');
+console.log(5 === '5'); 
+
+var v1 = parseFloat(window.prompt("Valor 1: ")); 
+var v2 = parseFloat(window.prompt("Valor 2: ")); 
+var v3 = parseFloat(v1 + v2);
+console.log(v3);
+//document.writeln("Valor1: " + v1 + "Valor2: " + v2 + "Resultado: " + v3); 
+
